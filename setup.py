4c2e9d390dadbbaabde509ce2ad86065144be51d
Tomotaka Ito <tomotaka.ito@gmail.com>
2020-08-03 18:04:25 +0900

    bump version 0.0.1 => 0.2.0
    - Python3 support
    - pytest style tests, more coverable
    - new build_soxdata() helper function to build SoxData object with values easilly
    - add soxsender package to help sending data via XMPP(using SleekXMPP library)

--- setup.py
+++ setup.py
@@ -2,9 +2,14 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
+
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
+
 setup(
     name='pysox',
-    version='0.0.1',
+    version='0.2.0',
     description='Sensor Over XMPP library for python',
     author='Hide. Tokuda Laboratory',
     author_email='ht@ht.sfc.keio.ac.jp',
@@ -12,7 +17,7 @@ setup(
     packages=find_packages(),
     license=open('LICENSE').read(),
     include_package_data=True,
-    install_requires=[],
+    install_requires=_requires_from_file('requirements.txt'),
     tests_requires=['nose'],
     test_suite='nose.collector'
 )
